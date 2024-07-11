c7543c44be60bbe0c9c6f95167348f9131e16e63
Safi Ullah Khan <141447767+Safiullahgithub@users.noreply.github.com>
2024-07-11 16:38:23 +0500

    Add files via upload

---
+++ src/com/company/Stringbuilder48.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Stringbuilder48 {
+
+
+    public static void main(String[] args){
+      StringBuilder sb=new StringBuilder("keny"+ " ");
+      sb.append("khan");
+      String name= sb.toString();
+      System.out.println(name);
+
+
+    }
+}
