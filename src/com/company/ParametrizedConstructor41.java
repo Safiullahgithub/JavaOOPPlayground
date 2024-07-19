b0341fc59ac32b52f4c32bff606f337c05feb76b
Safi Ullah Khan <141447767+Safiullahgithub@users.noreply.github.com>
2024-07-19 19:47:56 +0500

    Add files via upload

---
+++ src/com/company/ParametrizedConstructor41.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ParametrizedConstructor41 {
+
+    public  static void main(String[] args){
+        // make an object of the class
+        Car  mycarobj=new Car("Yolv","blue",445);
+
+        mycarobj.display();
+
+    }
+
+
+}
